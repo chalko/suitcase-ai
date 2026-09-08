c38e73808fe886ecf3913ee51ce822f552b15025
luna-mayor-agent <luna-mayor-agent@moonbase.local>
2026-09-08 09:18:27 -0700

    feat(colt): update Talos to v1.13.10 and Kubernetes to v1.35.8

--- provision/colt_downloads.tf
+++ provision/colt_downloads.tf
@@ -11,7 +11,7 @@ resource "proxmox_virtual_environment_download_file" "talos_iso" {
   content_type        = "iso"
   datastore_id        = "local"
   node_name           = var.proxmox_node
-  url                 = "https://github.com/siderolabs/talos/releases/download/v1.9.1/metal-amd64.iso"
-  file_name           = "talos-v1.9.1-metal-amd64.iso"
+  url                 = "https://github.com/siderolabs/talos/releases/download/v1.13.10/metal-amd64.iso"
+  file_name           = "talos-v1.13.10-metal-amd64.iso"
   overwrite_unmanaged = true
 }
--- provision/colt_talos.tf
+++ provision/colt_talos.tf
@@ -1,15 +1,16 @@
 # Create dedicated cluster secrets for Camp Colt
 resource "talos_machine_secrets" "colt" {
-  talos_version = "v1.9.1"
+  talos_version = "v1.13.10"
 }
 
 # Generate machine configuration for colt-control-01
 data "talos_machine_configuration" "colt_controlplane" {
-  cluster_name     = "camp-colt-k8s"
-  cluster_endpoint = "https://10.82.0.10:6443"
-  machine_type     = "controlplane"
-  machine_secrets  = talos_machine_secrets.colt.machine_secrets
-  talos_version    = "v1.9.1"
+  cluster_name       = "camp-colt-k8s"
+  cluster_endpoint   = "https://10.82.0.10:6443"
+  machine_type       = "controlplane"
+  machine_secrets    = talos_machine_secrets.colt.machine_secrets
+  talos_version      = "v1.13.10"
+  kubernetes_version = "v1.35.8"
 
   config_patches = [
     yamlencode({
@@ -37,11 +38,12 @@ data "talos_machine_configuration" "colt_controlplane" {
 
 # Generate machine configuration for colt-worker-01
 data "talos_machine_configuration" "colt_worker" {
-  cluster_name     = "camp-colt-k8s"
-  cluster_endpoint = "https://10.82.0.10:6443"
-  machine_type     = "worker"
-  machine_secrets  = talos_machine_secrets.colt.machine_secrets
-  talos_version    = "v1.9.1"
+  cluster_name       = "camp-colt-k8s"
+  cluster_endpoint   = "https://10.82.0.10:6443"
+  machine_type       = "worker"
+  machine_secrets    = talos_machine_secrets.colt.machine_secrets
+  talos_version      = "v1.13.10"
+  kubernetes_version = "v1.35.8"
 
   config_patches = [
     yamlencode({
